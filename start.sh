dd4aed2cc5cb468fcd44c49af2d71bb16e999cb2
xhahi <109585709+xhahi@users.noreply.github.com>
2022-07-20 13:57:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/ccadmin1/Movie-Bot /Movie-Bot
+git clone -b master https://github.com/xhahi/Movie-Bot /Movie-Bot
 cd /Movie-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
